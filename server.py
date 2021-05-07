6ab1b5673c562194510ae2cd48fdc6c05a68a714
Emerji <74623571+Emerji@users.noreply.github.com>
2021-05-07 19:49:24 -0400

    Create server.py

---
+++ server.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3.6
+"""
+Python 3.6 or newer required.
+"""
+import json
+import os
+import stripe
+# This is your real test secret API key.
+stripe.api_key = "sk_test_51Io1VYEosDw1KI2wSO1ovGQPu6a0H5uJVmnrAGu2XciqyGZ8sm0qpMnBqeeV1mmFpwPhkqkCp2HZJVhTPHbBKjHY00RNKQamtB"
+
+from flask import Flask, render_template, jsonify, request
+
+
+app = Flask(__name__, static_folder=".",
+            static_url_path="", template_folder=".")
+
+
+def calculate_order_amount(items):
+    # Replace this constant with a calculation of the order's amount
+    # Calculate the order total on the server to prevent
+    # people from directly manipulating the amount on the client
+    return 1400
+
+
+@app.route('/create-payment-intent', methods=['POST'])
+def create_payment():
+    try:
+        data = json.loads(request.data)
+        intent = stripe.PaymentIntent.create(
+            amount=calculate_order_amount(data['items']),
+            currency='usd'
+        )
+
+        return jsonify({
+          'clientSecret': intent['client_secret']
+        })
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
+if __name__ == '__main__':
+    app.run()
